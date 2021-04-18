6b3371e0a7685c3c3e9b540af1fc2355dd8ae6ae
Andrei Macavei <andrei.macavei@tundra.com>
2021-04-18 13:32:21 +0200

    test embed

--- src/index.js
+++ src/index.js
@@ -15,6 +15,24 @@ client.on('message', (msg) => {
 
     if (message.content.startsWith("$kick")) {
         var member = message.mentions.members.first();
-        msg.reply(`${member}`);
+        msg.reply(`${member.tag}`);
     }
+
+    client.on('message', message => {
+        // If the message is "how to embed"
+        if (message.content === 'how to embed') {
+            // We can create embeds using the MessageEmbed constructor
+            // Read more about all that you can do with the constructor
+            // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+            const embed = new MessageEmbed()
+                // Set the title of the field
+                .setTitle('A slick little embed')
+                // Set the color of the embed
+                .setColor(0xff0000)
+                // Set the main content of the embed
+                .setDescription('Hello, this is a slick embed!');
+            // Send the embed to the same channel as the message
+            message.channel.send(embed);
+        }
+    });
 });
